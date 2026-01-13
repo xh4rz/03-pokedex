285813c82b9e6ca06d402bd66fa94415787ccac6
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-01-12 23:35:28 -0500

    82. Eliminar un Pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -77,8 +77,9 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
   }
 
   private handleExceptions(error: any) {
