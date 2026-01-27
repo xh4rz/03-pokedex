06ca8f3c21bceea40c2e42e1f02028b5d28fb532
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2026-01-27 05:26:49 -0500

    104. Configuration Loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,17 @@ import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(`${process.env.MONGODB!}`),
+    MongooseModule.forRoot(process.env.MONGODB!),
     PokemonModule,
     CommonModule,
     SeedModule,
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -16,7 +16,9 @@ export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+  ) {
+    console.log(process.env.DEFAULT_LIMIT);
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
